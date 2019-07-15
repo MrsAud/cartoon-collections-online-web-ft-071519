1156dd26db55d879fd7bc3fc86f146e292b22a55
Audrea Cook <audreacwork@gmail.com>
2019-07-15 21:26:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(calls)
-  calls.map { |word| word.upcase } 
+  calls.map { |word| word.capitalize } 
 end
 
 def long_planeteer_calls# code an argument here
