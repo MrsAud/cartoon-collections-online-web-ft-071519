0613897bbf13e04507eca861473017264b01b206
Audrea Cook <audreacwork@gmail.com>
2019-07-15 21:23:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,9 @@
 require 'pry'
 
 def roll_call_dwarves(dwarves)
-  # Your code here
+  dwarves.each_with_index do |name, index|
+    puts "#{index} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
